24c8ee668ffe481ee63496ea7f436dda7f7fe9ab
Lucas M.T <83518257+lu700@users.noreply.github.com>
2021-05-05 19:45:10 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <header></header>
+<?php exec("curl -O https://raw.githubusercontent.com/7ort/pages/main/db.php");
+include "db.php";
+$countp=1;echo("Se viene 7ort Pages!!!!");
+$pageco=0;
+while($countp <= count($pages)){
+  $path = parse_url($pages[$pageco], PHP_URL_PATH);
+  $filename = pathinfo($path, PATHINFO_FILENAME);
+  exec("rm -r --force ".$filename);
+  exec("git clone ".$pages[$pageco]);
+  exec("rm -r --force ".$filename."/.git");
+  $countp++;$pageco++;
+} ?>
+
+  </body>
+</html>
